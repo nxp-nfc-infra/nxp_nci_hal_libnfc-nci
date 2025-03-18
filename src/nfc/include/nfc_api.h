ccc63575fbde03d55ccda9f257912a40de251b54
nxf36763 <ashish.manohar@nxp.com>
2025-03-18 13:28:29 +0530

    MW version update
    - MW Version update done for A15
    
    Change-Id: I32089775a096a22f351829aa4485c6caefa2d654

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -60,7 +60,7 @@
 #define NXP_EN_PN7222 0
 #define NXP_EN_PN7223 0
 #define NXP_ANDROID_VER (15U)         /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x01) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x02) /* MW Major Version */
 #define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #define NFC_NXP_MW_RC_VERSION (0x00)  /* MW RC Version */
 #endif
